ff6706a1d0066a9950ef080169abb0b137ed7500
Dominic Hupp <dhupp2020@gmail.com>
2020-09-17 19:42:01 -0400

    Added pfp upload support

--- web/src/components/SocialMediaBar.tsx
+++ web/src/components/SocialMediaBar.tsx
@@ -1,8 +1,9 @@
 import Box from '@material-ui/core/Box';
 import React, {ChangeEvent} from 'react';
-import {Divider, Grid, Hidden, Tooltip, Typography} from '@material-ui/core';
+import {Divider, Grid, Hidden, Tooltip, Typography, Dialog, DialogContent, DialogTitle, DialogContentText, DialogActions, Button} from '@material-ui/core';
 import instagramLogo from '../../images/instagramLogo.png'
 import UserPasswordUpdateForm from "./UserChangeScreen/UserPasswordUpdateForm";
+import TextEntry from "./TextEntry";
 
 interface Props {
     logo: any;
@@ -19,13 +20,42 @@ const SocialMediaBar: React.FC<Props> = ({
     height,
                                              imgHeight
                                          }) => {
+
+    const [open, setOpen] = React.useState(false);
+    const [input, setInput] = React.useState('');
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
         <Box boxShadow={4} bgcolor='background.paper' m={1} p={0} height={height} width={width} borderRadius={18}>
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">{text}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Please enter your username below
+                    </DialogContentText>
+                    <Box width={"250px"}>
+                    <TextEntry onChange={e => setInput(e.target.value)} helperText={'Enter your username here'} label={text} required={true} fullWidth={true} error={false}/>
+                    </Box>
+                    </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                </DialogActions>
+            </Dialog>
             <Grid
                 container
                 direction="row"
                 justify="center"
                 alignItems="center"
+                onClick={handleClickOpen}
             >
                 <Box marginRight={1} marginTop={2}>
                     <img height={imgHeight} width={imgHeight} src={logo} alt={text} />
--- web/src/components/UserChangeScreen/UserSettingsBox.tsx
+++ web/src/components/UserChangeScreen/UserSettingsBox.tsx
@@ -1,6 +1,17 @@
 import Box from '@material-ui/core/Box';
-import React from 'react';
-import {Divider, Grid, Hidden, Tooltip, Typography} from '@material-ui/core';
+import React, { useState } from 'react';
+import {
+    Button, Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Divider,
+    Grid,
+    Hidden,
+    Tooltip,
+    Typography
+} from '@material-ui/core';
 import Blurb from '../Blurb';
 import UserChangeInfoForm from "./UserChangeInfoForm";
 import UserPasswordUpdateForm from "./UserPasswordUpdateForm";
@@ -9,10 +20,88 @@ import snapchatLogo from '../../images/snapchatLogo.png';
 import twitterLogo from '../../images/twitterLogo.png';
 import facebookLogo from '../../images/facebookLogo.png';
 import SocialMediaBar from "../SocialMediaBar";
+import TextEntry from "../TextEntry";
+import axios from "../../utils/axios";
+import CryptoJS from "crypto-js";
+import {CRYPTO_JS_SECRETS, ERRORS, LOCALSTORAGE} from "@app/common";
 
 const UserSettingsBox = () => {
+    const [open, setOpen] = React.useState(false);
+    const [imageName, setImageName] = useState<string>("");
+    const [imageUpload, setImageUpload] = useState<Blob>(new Blob());
+    const [imageError, setImageError] = useState<string>("");
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleImageChange = (event: any) => {
+        const imageData = event.target.files[0];
+        if (imageData !== undefined) {
+            setImageUpload(imageData);
+            setImageName(imageData.name);
+        } else setImageName("");
+
+        if (imageData.type.trim().substring(0, 5) !== "image") {
+            setImageName("");
+            setImageError("Upload Image File");
+        } else setImageError("");
+    };
+
+    const submitPhoto = (event: { preventDefault: () => void; }) => {
+        event.preventDefault();
+        let photoInput = document.getElementById('profile-picture');
+        // @ts-ignore
+        if (photoInput.files[0]) {
+            const formData = new FormData();
+            // @ts-ignore
+            const upload_file = photoInput.files[0];
+            formData.append('profile-picture', upload_file);
+            axios
+                .post('/api/user/upload-profile-picture', formData)
+                .then((res: any) => {
+
+                    console.log(res.data);
+
+                })
+                .catch((error: any) => {
+
+                });
+        }
+    }
+
     return (
         <Box boxShadow={4} bgcolor='background.paper' m={2} p={3} borderRadius={8}>
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Upload Profile Picture</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Please upload a .jpeg/.png to display to other users. NOTE: This must follow the user guidelines
+                    </DialogContentText>
+                    <Box width={"250px"}>
+                        <form onSubmit={submitPhoto}>
+                            <input type="file"
+                                   id="profile-picture" name="profile-picture"
+                                   accept="image/png, image/jpeg"
+                            />
+                            <input
+                                className="submit-input"
+                                type="submit"
+                                value="Upload"
+                            />
+                        </form>
+                    </Box>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                </DialogActions>
+            </Dialog>
             <Grid
                 container
                 direction='row'
@@ -27,14 +116,14 @@ const UserSettingsBox = () => {
                         alignItems="center"
                     >
                         <Hidden mdUp>
-                            <Tooltip title="Upload Profile Picture" arrow>
+                            <Tooltip title="Upload Profile Picture" arrow onClick={handleClickOpen}>
                                 <img
                                     src={"https://moonvillageassociation.org/wp-content/uploads/2018/06/default-profile-picture1.jpg"}
                                     style={{borderRadius: '50%'}} width={85}/>
                             </Tooltip>
                         </Hidden>
                         <Hidden smDown>
-                            <Tooltip title="Upload Profile Picture" arrow>
+                            <Tooltip title="Upload Profile Picture" arrow onClick={handleClickOpen}>
                                 <img
                                     src={"https://moonvillageassociation.org/wp-content/uploads/2018/06/default-profile-picture1.jpg"}
                                     style={{borderRadius: '50%'}} width={170}/>
@@ -64,8 +153,8 @@ const UserSettingsBox = () => {
                         alignItems='flex-start'
                     >
                     <SocialMediaBar logo={instagramLogo} imgHeight={25} text={'Instagram'} width={180} height={55}/>
-                    <SocialMediaBar logo={twitterLogo} imgHeight={25} text={'Twitter'} width={180} height={55}/>
-                        <SocialMediaBar logo={facebookLogo} imgHeight={25} text={'Facebook'} width={180} height={55}/>
+                    <SocialMediaBar logo={twitterLogo} imgHeight={20} text={'Twitter'} width={180} height={55}/>
+                    <SocialMediaBar logo={facebookLogo} imgHeight={25} text={'Facebook'} width={180} height={55}/>
                     <SocialMediaBar logo={snapchatLogo} imgHeight={25} text={'Snapchat'} width={180} height={55}/>
                     </Grid>
                 </Grid>
