8aa1a220c13cbc1692ba5de83c306d44f55b2c9e
AdvaithNair <advaithnair2@gmail.com>
2020-09-18 19:05:38 -0700

    started fix with image upload

--- web/src/components/UserChangeScreen/UploadPhoto.tsx
+++ web/src/components/UserChangeScreen/UploadPhoto.tsx
@@ -9,16 +9,19 @@ import {
   DialogTitle,
   Grid,
   Hidden,
-  Tooltip
+  Tooltip,
+  CircularProgress,
+  IconButton
 } from '@material-ui/core';
 import axios from '../../utils/axios';
 import { clearLoading, setLoading } from '../../context/loading';
 import STATE from '../../context/state';
 import { FILE_UPLOADS, ReducerContext } from '@app/common';
 import { UserContext } from '../../context/context';
+import PhotoCameraIcon from '@material-ui/icons/PhotoCamera';
 
 const UploadPhoto = () => {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
   const [imageName, setImageName] = useState<string>('');
   const [imageUpload, setImageUpload] = useState<Blob>(new Blob());
   const [imageError, setImageError] = useState<string>('');
@@ -48,11 +51,9 @@ const UploadPhoto = () => {
   // 'https://moonvillageassociation.org/wp-content/uploads/2018/06/default-profile-picture1.jpg
   const submitPhoto = (event: { preventDefault: () => void }) => {
     event.preventDefault();
-    let photoInput = document.getElementById(FILE_UPLOADS.PROFILE_PICTURE);
-    // @ts-ignore
+    let photoInput: any = document.getElementById(FILE_UPLOADS.PROFILE_PICTURE);
     if (photoInput.files[0]) {
       const formData = new FormData();
-      // @ts-ignore
       const upload_file = photoInput.files[0];
       formData.append(FILE_UPLOADS.PROFILE_PICTURE, upload_file);
       axios
@@ -62,7 +63,9 @@ const UploadPhoto = () => {
           dispatch({ type: STATE.SET_UPLOAD_IMAGE, payload: res.data });
           clearLoading(dispatch);
         })
-        .catch((error: any) => {});
+        .catch((error: any) => {
+          console.log(error);
+        });
     }
   };
 
@@ -99,7 +102,7 @@ const UploadPhoto = () => {
       </Dialog>
       <Grid container direction='row' justify='center' alignItems='center'>
         <Hidden mdUp>
-          <Tooltip
+          {/*<Tooltip
             title='Upload Profile Picture'
             arrow
             onClick={handleClickOpen}
@@ -110,10 +113,10 @@ const UploadPhoto = () => {
               width={85}
               height={85}
             />
-          </Tooltip>
+          </Tooltip>*/}
         </Hidden>
         <Hidden smDown>
-          <Tooltip
+          {/*<Tooltip
             title='Upload Profile Picture'
             arrow
             onClick={handleClickOpen}
@@ -123,7 +126,35 @@ const UploadPhoto = () => {
               style={{ borderRadius: '50%' }}
               width={170}
               height={170}
-            />
+          />
+          </Tooltip>*/}
+          <Tooltip
+            title='Update Profile Picture'
+            placement='bottom'
+            onClick={handleClickOpen}
+          >
+            <div
+              className='profile-card-image-container'
+              onClick={() => setOpen(true)}
+            >
+              <img
+                className='profile-card-image'
+                src={state.user.imageURL}
+                alt={state.user.username}
+              ></img>
+              <div className='profile-card-image-overlay'>
+                <IconButton
+                  style={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)'
+                  }}
+                >
+                  <PhotoCameraIcon fontSize='large' />
+                </IconButton>
+              </div>
+            </div>
           </Tooltip>
         </Hidden>
       </Grid>
